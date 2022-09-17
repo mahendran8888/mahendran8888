8b633a67dd747283cf964939b8f82617544c6fec
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:14:35 +0530

    Update Buy and sell Back-testing.py

--- Buy and sell Back-testing.py
+++ Buy and sell Back-testing.py
@@ -3,8 +3,7 @@ from datetime import datetime,date,time,timedelta
 import pandas as pd
 import numpy as np
 
-breeze = BreezeConnect(api_key="f16xtr13o33~7R5798Q92b93X8s296u5")
-breeze.generate_session(api_secret="T801035#4143N(H0iF514bX3304l492Y", session_token="1511407")
+Login here
 
 
 bnhistory = breeze.get_historical_data(interval="1minute", from_date="2022-08-17T07:00:00.000Z",
