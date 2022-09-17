2f8dd63ac493656dea9b17ceaa744121b1b7c872
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:28:34 +0530

    Update bn_sma_runcontinously.py

--- bn_sma_runcontinously.py
+++ bn_sma_runcontinously.py
@@ -3,9 +3,7 @@ import time
 import smacorrection
 from breeze_connect import BreezeConnect
 import numpy as np
-
-breeze = BreezeConnect(api_key="1kA440t7401177L329z1I79H5R0J49y1")
-breeze.generate_session(api_secret="~2211636ES2RD9462951909401o+e597", session_token="1536299")
+Login details here
 
 now = datetime.datetime.now()
 
