2ecfb5693287d7f2f3b88267a9f7258f56f90f82
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:43:25 +0530

    Update profit_adda.py

--- profit_adda.py
+++ profit_adda.py
@@ -1,9 +1,6 @@
 from alice_blue import *
 import pandas as pd
-access_token = AliceBlue.login_and_get_access_token(username='AB133347', password='pragati@3', twoFA='1985',
-                                                    api_secret='bb4azbDnhxfJ7bbTKfd4gWjN8yc3IOXLykCLOfqvrdU3ixAONzyqe6dT0qh4xaeZ',
-                                                    app_id='jvbqTE5sF8')
-alice = AliceBlue(username='AB133347', password='pragati@3', access_token=access_token)
+Login details here
 
 alice.
 
