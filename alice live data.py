f619fe695f38c3a0091c638c214b19e93ff5cca5
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:18:45 +0530

    Update alice live data.py

--- alice live data.py
+++ alice live data.py
@@ -1,8 +1,7 @@
 from alice_blue import *
 from time import sleep
 
-access_token = AliceBlue.login_and_get_access_token(username='AB133347', password='pragati@3', twoFA='1985', api_secret='bb4azbDnhxfJ7bbTKfd4gWjN8yc3IOXLykCLOfqvrdU3ixAONzyqe6dT0qh4xaeZ', app_id='jvbqTE5sF8')
-alice = AliceBlue(username='AB133347', password='pragati@3', access_token=access_token)
+Login details here
 
 socket_opened = False
 def event_handler_quote_update(message):
