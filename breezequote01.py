47e8c47cf92b078fdff3848d83cd168eff6fe426
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:38:02 +0530

    Update breezequote01.py

--- breezequote01.py
+++ breezequote01.py
@@ -3,8 +3,7 @@ def qt():
     import pandas as pd
     import xlwings as xw
     import datetime
-    isec = BreezeConnect(api_key="86!G0e2uq1&2896Nk545R*Xp4+324u77")
-    isec.generate_session(api_secret="985h10U579QzF6898n586a5x06r2h335", session_token="1462472")
+Login details here
 
     stockcode = ['BPL', 'ACC', 'BEML', 'ITC', 'ABB', 'TCS','LUPIN', 'ASTPOL', 'INDMAR', 'CADHEA', 'LAULAB', 'AUSMA', 'IPCLAB', 'MAHFIN', 'PIIND', 'DIXTEC', 'TORPHA', 'CIPLA', 'NESIND', 'VOLTAS', 'DEENIT', 'MARLIM', 'UNIBR', 'JINSP', 'SYNINT', 'MINLIM', 'KPITEC', 'FIRSOU']
     appended_data = []
