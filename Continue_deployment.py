d4ab6a377629bbf5053ea94d676acdd45e63e44f
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:15:22 +0530

    Update Continue_deployment.py

--- Continue_deployment.py
+++ Continue_deployment.py
@@ -4,8 +4,7 @@ import deployment0001
 from breeze_connect import BreezeConnect
 import numpy as np
 
-breeze = BreezeConnect(api_key="w388658u78t85712+T22!484t0092*81")
-breeze.generate_session(api_secret="y56&ZBD2n=3219002f`78PyZI888H251", session_token="1488474")
+Login details here
 
 now = datetime.datetime.now()
 
