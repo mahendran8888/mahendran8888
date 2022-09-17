505bdc067f826264c47f68c17ef0d16e8b2e7838
mahendran8888 <67140427+mahendran8888@users.noreply.github.com>
2022-09-17 17:32:33 +0530

    Update breeze_last_or_now.py

--- breeze_last_or_now.py
+++ breeze_last_or_now.py
@@ -4,9 +4,7 @@ import datetime
 from datetime import datetime, timedelta
 
 from breeze_connect import BreezeConnect
-
-breeze = BreezeConnect(api_key="9493XC574494gm2E591i039Qb2(p90e1")
-breeze.generate_session(api_secret="071876u)z486md346506_1256~3Q31&D", session_token="1471639")
+Login details here
 
 bnquote = breeze.get_quotes(stock_code="CNXBAN",exchange_code="NFO",expiry_date="2022-08-11T06:00:00.000Z",product_type="options",right="call",strike_price="38600")
 
